22f9772c1d65a20d66beccd0e43a065d6f0fc356
spriveen <rpeanuka@gmail.com>
2025-01-14 07:00:30 -0800

    update connectionstring

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username= "root";
+$password = "root";
+$db_name = "login";
+$conn = new mysqli($servername,$username,$password,$db_name,3307);
+if(conn->connect_error){
+    die("Connection failed".$conn->connect_error);
+}
+
+?>
