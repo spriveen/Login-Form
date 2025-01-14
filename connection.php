ee125b3f4b1e171629820a8fe4aa5fcb9e4bc9fb
spriveen <rpeanuka@gmail.com>
2025-01-14 09:24:27 -0800

    update connectionstring

--- connection.php
+++ connection.php
@@ -1,11 +1,12 @@
 <?php
 $servername = "localhost";
-$username= "root";
-$password = "root";
+$username= "riveen";
+$password = "admin";
 $db_name = "login";
-$conn = new mysqli($servername,$username,$password,$db_name,3307);
-if(conn->connect_error){
+$conn = new mysqli($servername, $username, $password, $db_name, 3307);
+if($conn->connect_error){
     die("Connection failed".$conn->connect_error);
 }
+echo "Connection Sucessful";
 
 ?>
